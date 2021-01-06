b4791a9ca8e98cc3014b87d1dade38910b163b2f
themm1 <madzin.m@gmail.com>
2021-01-06 14:48:43 +0100

    chrome linux path

--- scrape_func.py
+++ scrape_func.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,13 +7,16 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # chrome setup (open chrome, accept cookies)
 def chrome_setup():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    options.add_argument("headless")
-    driver = webdriver.Chrome(executable_path="./drivers/chromedriver.exe", options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://www.yahoo.com/")
     try:
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME,"agree"))).click()
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.NAME,"agree"))).click()
     except:
         pass
     return driver
