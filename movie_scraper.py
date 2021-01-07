8a809366301e9a24a7a34bec8f2a7f2ef27c8e10
themm1 <madzin.m@gmail.com>
2021-01-07 08:45:46 +0100

    get_numbers function

--- movie_scraper.py
+++ movie_scraper.py
@@ -64,9 +64,7 @@ class Scraper:
     def rating(self, by_x, html_element):
         try:
             data = element(self.driver, by_x, html_element)
-            numbers_list = [data.text[i] for i in range(len(data.text)) if data.text[i].isnumeric()]
-            number = "".join(numbers_list)
-            return number
+            return get_numbers(data.text)
         except:
             return "Rating unavailable"
 
