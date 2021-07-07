9303f639237999aa021b544eb6b284452e03e86f
themm1 <madzin.m@gmail.com>
2021-07-07 11:27:30 +0200

    faceit command added

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,7 @@ import random
 import discord
 from discord.ext import commands
 from wot import getWotStats, wotEmbed
+from faceit import faceit_embed, FaceitStats
 from movies import imdb_main, movie_embed, series_embed
 
 
@@ -13,11 +14,11 @@ client = commands.Bot(command_prefix="!", help_command=None)
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="!help for commands"))
     print("Bot is ready.")
- 
+
 @client.command(aliases=["movie", "film"])
-async def movies(ctx, *, film):
+async def movies(ctx, *, film): 
     try:
-        movie = imdb_main(film, "movie", API_KEY)
+        movie = imdb_main(film, "movie", KEYS['omdb'])
         embed = movie_embed(movie)
         await ctx.send(embed=embed)
     except Exception:
@@ -26,18 +27,30 @@ async def movies(ctx, *, film):
 @client.command(aliases=["serial", "series"])
 async def serials(ctx, *, series):
     try:
-        serial = imdb_main(series, "series", API_KEY)
+        serial = imdb_main(series, "series", KEYS['omdb'])
         embed = series_embed(serial)
         await ctx.send(embed=embed)
     except Exception:
         await ctx.send("Couldn't find the series")
-    
+
+@client.command(aliases=["faceit", "facieitcs"])
+async def faceit_csgo(ctx, name):
+    try:
+        faceit = FaceitStats(name, KEYS['faceit'])
+        embed = faceit_embed(faceit.player)
+        await ctx.send(embed=embed)
+    except Exception:
+        await ctx.send("Couldn't find the player")
+
 @client.command()
 async def wot(ctx, *, player_name):
-    player = getWotStats(player_name)
-    wot_embed = wotEmbed(player)
-    await ctx.send(embed=wot_embed)
-    
+    try:
+        player = getWotStats(player_name)
+        embed = wotEmbed(player)
+        await ctx.send(embed=embed)
+    except Exception:
+        await ctx.send("Couldn't find the player")
+
 @client.command(aliases=["rn", "randomnumber"])
 async def random_number(ctx, max_nubmer):
     try:
@@ -72,7 +85,7 @@ async def help(ctx):
         WoT player's stats\n
         inforamtion about the movie\n
         inforamtion about the series""", inline=True)
-    
+
     await ctx.send(embed=embed)
 
 @client.command(aliases=["math", "result"])
@@ -109,22 +122,25 @@ async def rgb(ctx, string):
             return ",".join(rgb_color)
         return ERROR_MESSAGE
 
-    if "," in string:
+    if string[0] == "#" or string[:2] == "0x":
         try:
-            await ctx.send(hexa(string))
+            await ctx.send(rgb(string))
         except Exception:
             await ctx.send(ERROR_MESSAGE)
     else:
         try:
-            await ctx.send(rgb(string))
+            await ctx.send(hexa(string))
         except Exception:
             await ctx.send(ERROR_MESSAGE)
 
 
 try:
-    from secret import TOKEN, API_KEY
+    from secret import KEYS
 except Exception:
-    TOKEN = os.environ["BOT_TOKEN"]
-    API_KEY = os.environ["API_KEY"]
+    KEYS = {
+        "discord": os.environ['BOT_TOKEN'],
+        "omdb": os.environ['API_KEY'],
+        "faceit": os.environ['FACEIT_KEY']
+    }
 
-client.run(TOKEN)
+client.run(KEYS['discord'])
---
+++ bot/faceit.py
@@ -0,0 +1,55 @@
+import discord
+import requests
+from pprint import pprint
+
+class FaceitStats:
+    def __init__(self, player_name, api_token):
+        self.player_name = player_name
+        self.base_url = "https://open.faceit.com/data/v4"
+
+        self.headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {api_token}"
+        }
+        self.player = self.get_player()
+        self.player.update(self.get_stats()['lifetime'])
+
+    def get_player(self):
+        results = requests.get(f"{self.base_url}/search/players", headers=self.headers, params={"nickname": self.player_name}).json()
+        player_id = results['items'][0]['player_id']
+        player = requests.get(f"{self.base_url}/players/{player_id}", headers=self.headers).json()
+        
+        if player['avatar'] == "":
+            player.pop("avatar")
+            player['avatar'] = "https://pbs.twimg.com/profile_images/1349712390628270081/KpMEtOII.png"
+
+        player['elo'] = player['games']['csgo']['faceit_elo']
+        player['level'] = player['games']['csgo']['skill_level']
+        return player
+
+    def get_stats(self):
+        stats = requests.get(f"{self.base_url}/players/{self.player['player_id']}/stats/csgo", headers=self.headers)
+        return stats.json()
+
+def faceit_embed(player):
+    embed = discord.Embed(title=player['nickname'], description=f"""
+    Steam: [**{player['steam_nickname']}**](https://steamcommunity.com/profiles/{player['steam_id_64']})
+    Faceit: [**{player['nickname']}**](https://www.faceit.com/en/players/{player['nickname']})""", color=0xFF0000)
+    embed.set_thumbnail(url=player['avatar'])
+
+    embed.add_field(name="ELO", value=f"{player['games']['csgo']['faceit_elo']} "\
+    f"(level {player['games']['csgo']['skill_level']})", inline=True)
+    embed.add_field(name="K/D", value=player['Average K/D Ratio'], inline=True)
+    embed.add_field(name="Winrate", value=f"{player['Win Rate %']}%", inline=True)
+
+    for i in range(len(player['Recent Results'])):
+        if player['Recent Results'][i] == "0":
+            player['Recent Results'][i] = "L"
+        else:
+            player['Recent Results'][i] = "W"
+
+    embed.add_field(name="Matches", value=player['Matches'], inline=True)
+    embed.add_field(name="Recent Results", value=" ".join(player['Recent Results']), inline=True)
+    embed.add_field(name="Longest Win Streak", value=player['Longest Win Streak'], inline=True)
+
+    return embed
